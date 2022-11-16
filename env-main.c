7269ac3ca304a2f5204bdb702d63a1c757bc4da0
RennersClouds <rennerclouds@gmail.com>
2022-11-16 14:44:15 +0000

    betty style update

--- env-main.c
+++ env-main.c
@@ -3,17 +3,23 @@
 /**
  * main - prints the environment ucing the env parameter from the main function
  *
+ * @ac: argument to main
+ * @**av: argument to main
+ * @**env: argument to main
+ * @env: argument to the main
+ * @av: argument to the main
  * Return: Always 0.
  */
 int main(int ac, char **av, char **env)
 {
-    unsigned int i;
+	unsigned int i;
 
-    i = 0;
-    while (env[i] != NULL)
-    {
-        printf("%s\n", env[i]);
-        i++;
-    }
-    return (0);
+	i = 0;
+
+	while (env[i] != NULL)
+	{
+		printf("%s\n", env[i]);
+		i++;
+	}
+	return (0);
 }
